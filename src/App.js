f106cefa30f5b7c0d32166c538b57716e341488b
unknown <subhamagrwl@gmail.com>
2022-07-20 16:17:42 +0530

    login&&signup using firebase

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import SignUp from './SignUp/SignUp';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+      <SignUp></SignUp>
+        
+     
+    </div>
+  );
+}
+
+export default App;
