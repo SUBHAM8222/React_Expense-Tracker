7987ec66c0d2b4f58bbc9399a99f21a4c9f4786c
unknown <subhamagrwl@gmail.com>
2022-07-21 21:31:54 +0530

    logout

--- src/Pages/WelcomePage.js
+++ src/Pages/WelcomePage.js
@@ -1,13 +1,89 @@
 import './WelcomePage.css';
-import { NavLink } from 'react-router-dom';
+//import { useState,} from 'react';
+import { NavLink,useHistory} from 'react-router-dom';
 const WelcomePage=()=>{
+    //const otpinputref=useRef();
+    const history=useHistory();
+
+//const [verifyemail,isverified]=useState(false);
+    const verifyemailhandler=()=>{
+       // isverified(true);
+        const token =localStorage.getItem('IDTOKEN');
+
+       fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDvw0A-v_9F2Unb81eL4qV9KSPayWoxBO4',
+       {
+
+        method: 'POST',
+        body:JSON.stringify({
+           
+        idToken:token,
+        requestType:'VERIFY_EMAIL',
+          }),
+          headers:{
+            'Content-Type': 'application/json'
+        
+       }
+    }
+       
+       ).then(res=>{
+        if(res.ok)
+        {
+            alert('check your email for verification');
+        }
+        else{
+            alert('aunthentication failed');
+        }
+       })
+    }
+
+    // const otpverifyhandler=()=>{
+    //     isverified(false);
+
+    //     const enteredOTP=otpinputref.current.value;
+
+    //     fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDvw0A-v_9F2Unb81eL4qV9KSPayWoxBO4',{
+    //         method: 'POST',
+    //     body:JSON.stringify({
+           
+    //         oobCode:enteredOTP,
+    //       }),
+    //       headers:{
+    //         'Content-Type': 'application/json'
+        
+       //}
 
 
+// 
+    //     }).then(res=>{
+    //         if(res.ok)
+    //         {
+    //             console.log('not verifed');
+    //             alert('Error');
+    //         }
+    //     })
+
+    // }
+
+    const logoutHandler=()=>{
+        localStorage.removeItem('IDTOKEN');
+
+        history.replace('/');
+    }
     return(
 <div>
         <div className='style'>
             WElCOME TO EXPENSE TRACKER
-            <div className='style2'>YOUR profile is incomplete <NavLink to='/CompleteProfile'>complete Now</NavLink></div>
+            <button style={{color:'black',backgroundColor:'greblacky'}} onClick={verifyemailhandler}>verify Email</button>
+           {/* { verifyemail&&<form>
+                <input type='number' ref={otpinputref}></input>
+                <button onClick={otpverifyhandler}>EnterOTP</button>
+            </form>} */}
+            <div className='style2'>YOUR profile is incomplete <NavLink to='/CompleteProfile'>complete Now</NavLink>
+
+            </div>
+            <div>
+                <button onClick={logoutHandler}>LOGOUT</button>
+            </div>
         </div>
        
         </div>
